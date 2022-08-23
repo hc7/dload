98c0bfc557941e460c096107535befa58cc928ac
v.agafonov <hcs@inbox.ru>
2022-08-23 18:46:14 +0300

    fixed

--- Core/Bespin.cpp
+++ Core/Bespin.cpp
@@ -1,34 +1,5 @@
 #include "Bespin.h"
 
-#if defined(__linux__) || defined(__APPLE__)
-extern "C"
-{
-	Bespin *allocator()
-	{
-		return new Bespin();
-	}
-
-	void deleter(Bespin *ptr)
-	{
-		delete ptr;
-	}
-}
-#endif
-
-#ifdef WIN32
-extern "C"
-{
-	__declspec (dllexport) Bespin *allocator()
-	{
-		return new Bespin();
-	}
-
-	__declspec (dllexport) void deleter(Bespin *ptr)
-	{
-		delete ptr;
-	}
-}
-#endif
 
 void Bespin::greet()
 {
